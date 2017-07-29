68834a36b201c6b3b94ecc9e13589700be610bbf
yanghu <huyang@mail.bistu.edu.cn>
2017-07-29 12:41:22 +0800

    done

--- quickSort/main.m
+++ quickSort/main.m
@@ -8,9 +8,31 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+void quickSort(int a[],int leftIndx,int rightIndex) {
+    if (leftIndx < rightIndex) {
+        int x = a[leftIndx], i = leftIndx, j = rightIndex;
+        while (i < j) {
+            while (i < j && a[j] >= x) j--;
+            if (i < j) a[i++] = a[j];
+            
+            while (i < j && a[i] < x) i++;
+            if (i < j) a[j--] = a[i];
+        }
+        a[i] = x;
+        
+        quickSort(a, leftIndx, i-1);
+        quickSort(a, i+1, rightIndex);
+    }
+}
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        int a[10] = {4,2,1,6,7,3,8,9,0,5};
+        quickSort(a, 0, 9);
+        for (int i=0; i<10; i++) {
+            printf("%d,",a[i]);
+        }
+        
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
