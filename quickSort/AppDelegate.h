73708cdcbe7302f54babc020f4fb9b578d1ec188
yanghu <huyang@mail.bistu.edu.cn>
2017-07-29 12:32:39 +0800

    initialize

---
+++ quickSort/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  quickSort
+//
+//  Created by young on 2017/7/29.
+//  Copyright © 2017年 young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ quickSort/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  quickSort
+//
+//  Created by young on 2017/7/29.
+//  Copyright © 2017年 young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
